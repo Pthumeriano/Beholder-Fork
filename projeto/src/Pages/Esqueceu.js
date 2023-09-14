72e0098ae423a31c2cba9ccd64b762f407e21460
JasCastro21 <jasminecastr0@edu.unifor.br>
2023-09-14 00:10:13 -0300

    Tela de Esqueceu Senha

--- projeto/src/Pages/Esqueceu.js
+++ projeto/src/Pages/Esqueceu.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/Esqueceu.css';
+import logo from '../img/logo.png';
 
-function Esqueceu() {
-  return (
-    <div>Esqueceu</div>
-  )
+class ForgotPasswordForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailOuNumero: ''
+    };
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode adicionar a lógica para recuperar a senha
+  }
+
+  render() {
+    return (
+      <div className="login-page">
+        <div className="login-form">
+          <div className="logo-container">
+            <img src={logo} alt="Logo" style={{ width: '208px', height: '208px' }} />
+            <h2>Recuperar Senha</h2>
+          </div>
+
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                name="emailOuNumero"
+                placeholder="E-mail ou Número"
+                value={this.state.emailOuNumero}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+
+            <button type="submit" className="submit-button">Solicitar Código</button>
+          </form>
+
+          <div className="resend-code">
+            Não recebeu? <Link to="/solicitar-novamente">Solicitar Novamente</Link>
+          </div>
+
+          <div className="signup-link">
+            Lembrou da senha? <Link to="/entrar">Entre aqui</Link>!
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Esqueceu
+export default ForgotPasswordForm;
