e4753766b8fee05f7568a17c07d23cd4be206c9b
JasCastro21 <jasminecastr0@edu.unifor.br>
2023-10-06 20:49:11 -0300

    feedpage

--- projeto/src/components/Feed.js
+++ projeto/src/components/Feed.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import '../Styles/Feed.css';
 import { FaCamera, FaSmile } from 'react-icons/fa'; 
+import Chu from '../img/chuu2.jpg';
 
 const Feed = ({ posts }) => {
   return (
     <div className="feed-container">
       <div className="post-bar">
-        <img className="user-avatar" src="url-da-imagem-do-avatar" alt="User Avatar" />
-        <input type="text" placeholder="Compartilhe novidade das suas mesas!" />
-        <div className="post-icons">
-          <FaCamera />  
-          <FaSmile />   
+        <div className="post-input-container">
+          <img className="user-avatar" src={Chu} alt="User Avatar" />
+          <input className="post-input" type="text" placeholder='Compartilhe novidade das suas mesas!' />
         </div>
-        <button>Postar</button>
+        <div className="post-icons">
+          <FaCamera className="icon-camera" />  
+          <FaSmile className="icon-smile" />   
+        </div>
+        <button className="post-button">Postar</button>
       </div>
-      
       {/* ... resto do componente de posts ... */}
     </div>
   );
