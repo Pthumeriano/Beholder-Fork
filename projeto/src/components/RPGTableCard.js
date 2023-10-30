156187cb59000c97dd04a142f36757a08b469db8
JasCastro21 <jasminecastr0@edu.unifor.br>
2023-10-30 19:30:03 -0300

    tela de mesas pronta

--- projeto/src/components/RPGTableCard.js
+++ projeto/src/components/RPGTableCard.js
@@ -1,36 +1,43 @@
 import React from 'react';
 import { FaStar, FaRegStar, FaUser, FaBookOpen, FaClock, FaCalendar, FaDollarSign } from 'react-icons/fa';
-import '../Styles/CardRPG.css'
+import '../Styles/CardRPG.css';
 
 function RPGTableCard({ tableData }) {
   const joinTable = (tableId) => {
     console.log(`Você está entrando na mesa com o ID ${tableId}`);
   };
 
+  const createTable = () => {
+    console.log('Criando uma nova mesa');
+  };
+
   return (
-    <div className="card-contenttable">
-      <div className="table-image-container">
-        <img
-          src={tableData.imagePath}
-          alt={tableData.title}
-          className="table-image"
-        />
-        <div className="stars">
-          {[...Array(5)].map((_, i) => i < tableData.rating ? <FaStar key={i} /> : <FaRegStar key={i} />)}
+    <>
+      <div className="card-contenttable">
+        <div className="table-image-container">
+          <img
+            src={tableData.imagePath}
+            alt={tableData.title}
+            className="table-image"
+          />
+          <div className="stars">
+            {[...Array(5)].map((_, i) => i < tableData.rating ? <FaStar key={i} /> : <FaRegStar key={i} />)}
+          </div>
         </div>
+        <div className="table-details">
+          <h3>{tableData.title}</h3>
+          <span className="tag">{tableData.tag}</span>
+          <p><FaUser /> Mestre: {tableData.dungeonMaster}</p>
+          <p><FaBookOpen /> Vagas: {tableData.vagas}</p>
+          <p><FaClock /> Duração: {tableData.duration}</p>
+          <p><FaCalendar /> Horário: {tableData.horario}</p>
+          <p><FaDollarSign /> Valor: {tableData.valor}</p>
+          <p>{tableData.description}</p>
+        </div>
+        <button className="button" onClick={() => joinTable(tableData.id)}>Enviar Pedido</button>
       </div>
-      <div className="table-details">
-        <h3>{tableData.title}</h3>
-        <span className="tag">{tableData.tag}</span>
-        <p><FaUser /> Mestre: {tableData.dungeonMaster}</p>
-        <p><FaBookOpen /> Vagas: {tableData.vagas}</p>
-        <p><FaClock /> Duração: {tableData.duration}</p>
-        <p><FaCalendar /> Horário: {tableData.horario}</p>
-        <p><FaDollarSign /> Valor: {tableData.valor}</p>
-        <p>{tableData.description}</p>
-      </div>
-      <button className="button" onClick={() => joinTable(tableData.id)}>Enviar Pedido</button>
-    </div>
+      <button className="button create-table-button" onClick={createTable}>Criar Mesa</button>
+    </>
   );
 }
 
