74394ba223f158efdf21fe6798730c48c8c9176f
Helder Andrade <helderandrade377@gmail.com>
2023-11-25 17:31:27 -0300

    Perfil aparentemente corrigido

--- projeto/src/components/UserProfile.js
+++ projeto/src/components/UserProfile.js
@@ -1,3 +1,5 @@
+// ... (other imports)
+
 import coverPic from '../img/06.png';  
 import profilePic from '../img/chuu2.jpg';  
 import globeIcon from '../img/globe.png';  
@@ -7,9 +9,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as JWT from 'jwt-decode';
 
-
-// Função para obter o valor de um cookie pelo nome
-function getCookieValue(nome){
+function getCookieValue(nome) {
   const cookies = document.cookie.split('; ');
   for (const cookie of cookies) {
     const [name, value] = cookie.split('=');
@@ -17,38 +17,20 @@ function getCookieValue(nome){
       return value;
     }
   }
-  return null; // Retorna null se o cookie não for encontrado
-};
+  return null;
+}
 
 function UserProfile() {
-
   const [activeTab, setActiveTab] = useState("UserProfile");
-  const [userData, setUserData] = useState({
-    id: '',
-    nome: '',
-    email: '',
-    senha: '',
-    datanascimento: null,
-    telefone: null,
-    descricao: null,
-    xp: 0,
-    criado_em: '',
-  });
+  const [userData, setUserData] = useState([]);
 
   useEffect(() => {
-    // Função para buscar os dados do usuário
     const fetchUserData = async () => {
       try {
-        
-        // Obtenha o token do cookie BeholderToken
         const token = getCookieValue('BeholderToken');
-  
-        // Se o token existir, decodifique-o para obter o userId
         if (token) {
           const decodedToken = JWT.jwtDecode(token);
           const userId = decodedToken.userId;
-
-          // Faça a solicitação para obter os dados do usuário usando o userId
           const response = await axios.get(`http://localhost:4200/api/usuario/${userId}`);
           setUserData(response.data);
         }
@@ -56,24 +38,28 @@ function UserProfile() {
         console.error('Erro ao obter os dados do usuário:', error);
       }
     };
-  
-    // Chama a função para buscar os dados do usuário
+
     fetchUserData();
   }, []);
-  
+
   return (
-    
     <div className="perfil-container">
       <img src={coverPic} alt="Foto de Capa" className="cover-pic"/>
       <img src={profilePic} alt="Foto de Perfil" className="profile-pic"/>
       <div className="profile-content">
-        <div className="header-content">
-          <h2>{userData[0].nome}</h2>
-          <button className="edit-profile-button">Editar Perfil</button>
-        </div>
-        <p className='user-tag'>{`email: ${userData[0].email}`}</p>
+        {userData.length > 0 ? (
+          <div className="header-content">
+            <h2>{userData[0].nome}</h2>
+            <button className="edit-profile-button">Editar Perfil</button>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+        {userData.length > 0 && userData[0].email && (
+          <p className='user-tag'>{`Email: ${userData[0].email}`}</p>
+        )}
         <p className='user-info'>
-          {userData[0].descricao || "Nenhuma descrição disponível"}
+          {userData.length > 0 && userData[0].descricao || "Nenhuma descrição disponível"}
         </p>
         <div className="details">
           <div className="detail">
@@ -86,17 +72,17 @@ function UserProfile() {
           </div>
         </div>
         <div className="stats">
-        <div className="stat">
-          <p>23</p>
-          <p>Seguindo</p>
-        </div>
-        <div className="stat">
-          <p>56</p>
-          <p>Seguidores</p>
+          <div className="stat">
+            <p>23</p>
+            <p>Seguindo</p>
+          </div>
+          <div className="stat">
+            <p>56</p>
+            <p>Seguidores</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="posts-container">
+      <div className="posts-container">
       <div className="feed-header">
         <div
           className={`tab ${activeTab === "posts" ? "active clickable" : "clickable"}`}
