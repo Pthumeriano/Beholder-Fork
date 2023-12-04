cafd6670394eb2fee5b46ab9556e544d295bfb81
Helder Andrade <helderandrade377@gmail.com>
2023-12-04 07:11:40 -0300

    View de mesas possivelmente completa

--- projeto/src/components/UserProfile.js
+++ projeto/src/components/UserProfile.js
@@ -1,18 +1,19 @@
-// ... (other imports)
-
-import coverPic from '../img/06.png';  
-import profilePic from '../img/chuu2.jpg';  
-import globeIcon from '../img/globe.png';  
-import linkIcon from '../img/link.png';  
-import '../Styles/UserProfile.css';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import * as JWT from 'jwt-decode';
+import RPGTableCard from "../components/RPGTableCard";
+import coverPic from "../img/06.png";
+import profilePic from "../img/chuu2.jpg";
+import globeIcon from "../img/globe.png";
+import linkIcon from "../img/link.png";
+import "../Styles/UserProfile.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as JWT from "jwt-decode";
+import { useSearch } from "../contexts/SearchContext";
+import { getMinhasMesas } from "../services/api/mesa";
 
 function getCookieValue(nome) {
-  const cookies = document.cookie.split('; ');
+  const cookies = document.cookie.split("; ");
   for (const cookie of cookies) {
-    const [name, value] = cookie.split('=');
+    const [name, value] = cookie.split("=");
     if (name === nome) {
       return value;
     }
@@ -23,29 +24,51 @@ function getCookieValue(nome) {
 function UserProfile() {
   const [activeTab, setActiveTab] = useState("UserProfile");
   const [userData, setUserData] = useState([]);
+  const [userMesas, setUserMesas] = useState([]);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const token = getCookieValue('BeholderToken');
+        const token = getCookieValue("BeholderToken");
         if (token) {
           const decodedToken = JWT.jwtDecode(token);
           const userId = decodedToken.userId;
-          const response = await axios.get(`http://localhost:4200/api/usuario/${userId}`);
+          const response = await axios.get(
+            `http://localhost:4200/api/usuario/${userId}`
+          );
           setUserData(response.data);
         }
       } catch (error) {
-        console.error('Erro ao obter os dados do usuário:', error);
+        console.error("Erro ao obter os dados do usuário:", error);
+      }
+    };
+
+    const fetchUserMesas = async () => {
+      try {
+        const token = getCookieValue("BeholderToken");
+        if (token) {
+          const decodedToken = JWT.jwtDecode(token);
+          const userId = decodedToken.userId;
+
+          const response = await getMinhasMesas(userId);
+
+          setUserMesas(response.data);
+        }
+      } catch (error) {
+        console.error("Erro ao obter as mesas do usuário:", error);
       }
     };
 
     fetchUserData();
+    fetchUserMesas();
   }, []);
+
+  const { search } = useSearch();
 
   return (
     <div className="perfil-container">
-      <img src={coverPic} alt="Foto de Capa" className="cover-pic"/>
-      <img src={profilePic} alt="Foto de Perfil" className="profile-pic"/>
+      <img src={coverPic} alt="Foto de Capa" className="cover-pic" />
+      <img src={profilePic} alt="Foto de Perfil" className="profile-pic" />
       <div className="profile-content">
         {userData.length > 0 ? (
           <div className="header-content">
@@ -56,10 +79,11 @@ function UserProfile() {
           <p>Loading...</p>
         )}
         {userData.length > 0 && userData[0].email && (
-          <p className='user-tag'>{`Email: ${userData[0].email}`}</p>
+          <p className="user-tag">{`Email: ${userData[0].email}`}</p>
         )}
-        <p className='user-info'>
-          {userData.length > 0 && userData[0].descricao || "Nenhuma descrição disponível"}
+        <p className="user-info">
+          {(userData.length > 0 && userData[0].descricao) ||
+            "Nenhuma descrição disponível"}
         </p>
         <div className="details">
           <div className="detail">
@@ -83,73 +107,84 @@ function UserProfile() {
         </div>
       </div>
       <div className="posts-container">
-      <div className="feed-header">
-        <div
-          className={`tab ${activeTab === "posts" ? "active clickable" : "clickable"}`}
-          onClick={() => setActiveTab("posts")}
-        >
-          Posts
-        </div>
+        <div className="feed-header">
+          <div
+            className={`tab ${
+              activeTab === "posts" ? "active clickable" : "clickable"
+            }`}
+            onClick={() => setActiveTab("posts")}
+          >
+            Posts
+          </div>
 
-        <div
-          className={`tab ${activeTab === "respostas" ? "active clickable" : "clickable"}`}
-          onClick={() => setActiveTab("respostas")}
-        >
+          <div
+            className={`tab ${
+              activeTab === "respostas" ? "active clickable" : "clickable"
+            }`}
+            onClick={() => setActiveTab("respostas")}
+          >
+            Respostas
+          </div>
 
-          Respostas
+          <div
+            className={`tab ${
+              activeTab === "mesas" ? "active clickable" : "clickable"
+            }`}
+            onClick={() => setActiveTab("mesas")}
+          >
+            Mesas
+          </div>
 
-         </div>
-
-        <div
-          className={`tab ${activeTab === "mesas" ? "active clickable" : "clickable"}`}
-          onClick={() => setActiveTab("mesas")}
-        >
-          Mesas
-         </div>
-
-         <div
-          className={`tab ${activeTab === "curtidas" ? "active clickable" : "clickable"}`}
-          onClick={() => setActiveTab("curtidas")}
-        >
-          Curtidas
-         </div>
-
+          <div
+            className={`tab ${
+              activeTab === "curtidas" ? "active clickable" : "clickable"
+            }`}
+            onClick={() => setActiveTab("curtidas")}
+          >
+            Curtidas
+          </div>
         </div>
       </div>
 
-      {activeTab === 'posts' && (
-        <div>
-          aaaa
-        </div>
-      )
-        
-      }
+      {activeTab === "posts" && <div>aaaa</div>}
 
-      {activeTab === 'respostas' && (
-        <div>
-          bbbb
-        </div>
-      )
-        
-      }
+      {activeTab === "respostas" && <div>bbbb</div>}
 
-      {activeTab === 'mesas' && (
-        <div>
-          cccc
-        </div>
-      )
-        
-      }
-      
-      {activeTab === 'curtidas' && (
-        <div>
-          dddd
-        </div>
-      )
-        
-      }
+      {activeTab === "mesas" && (
+        <>
+          {activeTab === "mesas" && (
+            <>
+              {userMesas
+                .filter((mesa) =>
+                  mesa.titulo.toLowerCase().includes(search.toLowerCase())
+                )
+                .map((mesa) => (
+                  <RPGTableCard
+                    key={mesa.id}
+                    tableData={{
+                      id: mesa.id,
+                      title: mesa.titulo,
+                      subtitle: mesa.subtitulo,
+                      system: mesa.sistema,
+                      description: mesa.descricao,
+                      createdOn: mesa.criado_em,
+                      date: mesa.dia,
+                      time: mesa.horario,
+                      period: mesa.periodo,
+                      price: mesa.preco,
+                      vacancies: mesa.vagas,
+                      dungeonMasterName: userData[0].nome,
+                      chatId: mesa.chat,
+                    }}
+                  />
+                ))}
+            </>
+          )}
+        </>
+      )}
+
+      {activeTab === "curtidas" && <div>dddd</div>}
     </div>
-    
   );
 }
 
--- projeto/src/services/api/mesa.js
+++ projeto/src/services/api/mesa.js
@@ -8,6 +8,10 @@ export const getMesas = async () => {
   return await api.get("/mesas");
 };
 
+export const getMinhasMesas = async () => {
+  return await api.get("/minhasmesas", { withCredentials: true });
+};
+
 export const getMesa = async (id) => {
   return await api.get(`/mesa/${id}`);
 };
